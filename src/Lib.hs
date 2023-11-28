31b0ab96da62bcdba8271202ee51a1b50dafbfbd
jouzep <josephyu31@gmail.com>
2023-11-29 00:44:04 +0100

    test

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,33 @@ module Lib
     ( someFunc
     ) where
 
+data SExpr 
+    = IntExpression Int
+    | SymbolExpression String
+    | ListExpress [SExpr]
+    deriving Show
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- printTree :: SExpr -> Maybe String
+
+getSymbol :: SExpr -> Maybe String
+getSymbol (SymbolExpression symbol) = Just symbol
+getSymbol _             = Nothing
+
+-- printTree :: SExpr -> Maybe String
+-- getSymbol (SymbolExpression symbol) = Just symbol
+-- getSymbol (SymbolExpression symbol) = Just symbol
+-- getSymbol (SymbolExpression symbol) = Just symbol
+-- getSymbol _ = Nothing
+
+printTree :: SExpr -> Maybe String
+printTree (IntExpression n) = Just $ "an Integer " ++ show n
+printTree (SymbolExpression s) = Just $ "a Symbol '" ++ s ++ "'"
+printTree (ListExpress []) = Nothing -- Empty list, handle as needed
+printTree (ListExpress [expr]) = printTree expr
+printTree (ListExpress (expr : rest)) =
+  case (printTree expr, printTree (ListExpress rest)) of
+    (Just str1, Just str2) -> Just $ "a List with " ++ str1 ++ ", " ++ str2
+    _                      -> Nothing
