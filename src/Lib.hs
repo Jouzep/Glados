a52414dd3d02219de3b3e987953ed8ba746fca60
jouzep <josephyu31@gmail.com>
2023-11-29 11:36:39 +0100

    printTree

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,18 +17,19 @@ getSymbol :: SExpr -> Maybe String
 getSymbol (SymbolExpression symbol) = Just symbol
 getSymbol _             = Nothing
 
--- printTree :: SExpr -> Maybe String
--- getSymbol (SymbolExpression symbol) = Just symbol
--- getSymbol (SymbolExpression symbol) = Just symbol
--- getSymbol (SymbolExpression symbol) = Just symbol
--- getSymbol _ = Nothing
-
 printTree :: SExpr -> Maybe String
 printTree (IntExpression n) = Just $ "an Integer " ++ show n
 printTree (SymbolExpression s) = Just $ "a Symbol '" ++ s ++ "'"
-printTree (ListExpress []) = Nothing -- Empty list, handle as needed
+printTree (ListExpress []) = Nothing
 printTree (ListExpress [expr]) = printTree expr
 printTree (ListExpress (expr : rest)) =
   case (printTree expr, printTree (ListExpress rest)) of
     (Just str1, Just str2) -> Just $ "a List with " ++ str1 ++ ", " ++ str2
     _                      -> Nothing
+
+printTree :: SExpr -> Maybe String
+printTree (IntExpression n) = Just $ "an Integer " ++ show n
+printTree (SymbolExpression s) = Just $ "a Symbol '" ++ s ++ "'"
+printTree (ListExpress []) = Nothing
+printTree (ListExpress [expr]) = printTree expr
+printTree (ListExpress (expr : rest)) = Just $ "a List with " ++ printTree expr ++ ", " ++ printTree (ListExpress rest)
