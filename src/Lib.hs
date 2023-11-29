d97aff5b85f329a3346f4c5741d9b4466ddfe9c1
jouzep <josephyu31@gmail.com>
2023-11-29 23:38:32 +0100

    feat(test)

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,22 +9,15 @@ data SExpr
     deriving Show
 
 
--- data Ast = Define ? ?
--- | ? Int
--- ...
--- deriving Show
-
 data Ast
   = Define  String Ast
   | IntLiteral Int
-  | Symbol { symbolValue :: String }
-  | Boolean { boolValue :: Bool }
+  | Symbol  String
+  | Boolean  Bool
   deriving (Show)
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
-
--- printTree :: SExpr -> Maybe String
 
 getSymbol :: SExpr -> Maybe String
 getSymbol (SymbolExpression symbol) = Just symbol
@@ -40,16 +33,12 @@ printTree (ListExpress (expr : rest)) =
     (Just str1, Just str2) -> Just $ "a List with " ++ str1 ++ ", " ++ str2
     _                      -> Nothing
 
--- printTree :: SExpr -> Maybe String
--- printTree (IntExpression n) = Just $ "an Integer " ++ show n
--- printTree (SymbolExpression s) = Just $ "a Symbol '" ++ s ++ "'"
--- printTree (ListExpress []) = Nothing
--- printTree (ListExpress [expr]) = printTree expr
--- printTree (ListExpress (expr : rest)) = Just $ "a List with " ++ printTree expr ++ ", " ++ printTree (ListExpress rest)
-
 sexprToAST :: SExpr -> Maybe Ast
 sexprToAST (IntExpression n) = Just (IntLiteral n)
 sexprToAST (SymbolExpression n) = Just (Symbol n)
-sexprToAST (SymbolExpression "Define") = Just (Define "Define" (sexprToAST n))
-sexprToAST (ListExpress [n]) = sexprToAST n
+sexprToAST (ListExpress [SymbolExpression "define", SymbolExpression var, expr]) =
+  case sexprToAST expr of
+    Just astExpr -> Just (Define var astExpr)
+    Nothing      -> Nothing
+sexprToAST _ = Nothing
 _ = Nothing
