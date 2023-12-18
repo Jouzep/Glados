a0facbae38f39f350496fcc7e3f98eec6d435be3
Antonyjin <antony.jin@epitech.eu>
2023-12-19 00:10:17 +0100

    feat(SList Conversion): Convert SExpr to AST for Define #8

--- src/AST/Tree/ConvertToAst.hs
+++ src/AST/Tree/ConvertToAst.hs
@@ -7,5 +7,7 @@ import AST.Constants
 convertSExprToAst :: SExpr -> Ast
 convertSExprToAst (SInt n) = Var (AstInt n)
 convertSExprToAst (SSymb s) = Var (AstSymb s)
+convertSExprToAst (SList [SSymb "define", SSymb name, valueExpr]) =
+    Define name (convertSExprToAst valueExpr)
 convertSExprToAst _ = error "Invalid SExpr"
 
