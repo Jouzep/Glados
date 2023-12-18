b4e2ebebde08ecf490e1a9bb268ed6bdfe702126
Antonyjin <antony.jin@epitech.eu>
2023-12-19 00:16:43 +0100

    feat(SList binary op conversion): Convert SExpr to AST for binary opertors #8

--- src/AST/Tree/ConvertToAst.hs
+++ src/AST/Tree/ConvertToAst.hs
@@ -9,5 +9,22 @@ convertSExprToAst (SInt n) = Var (AstInt n)
 convertSExprToAst (SSymb s) = Var (AstSymb s)
 convertSExprToAst (SList [SSymb "define", SSymb name, valueExpr]) =
     Define name (convertSExprToAst valueExpr)
+convertSExprToAst (SList [SSymb operator, operand1, operand2]) =
+    BinaryOp (stringToBinaryOp operator) (convertSExprToAst operand1) (convertSExprToAst operand2)
 convertSExprToAst _ = error "Invalid SExpr"
 
+stringToBinaryOp :: String -> AstBinaryOp
+stringToBinaryOp "+" = Add
+stringToBinaryOp "*" = Mul
+stringToBinaryOp "-" = Sub
+stringToBinaryOp "/" = Div
+stringToBinaryOp "%" = Mod
+stringToBinaryOp "=" = Eq
+stringToBinaryOp "/=" = NotEq
+stringToBinaryOp "<" = Less
+stringToBinaryOp "<=" = LessOrEq
+stringToBinaryOp ">" = Greater
+stringToBinaryOp ">=" = GreaterOrEq
+stringToBinaryOp "&&" = And
+stringToBinaryOp "||" = Or
+stringToBinaryOp _ = error "Invalid binary operator"
