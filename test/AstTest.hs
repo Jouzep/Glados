5e4d464cf24a8aa04fc156a74b65b68f30653e4f
Antonyjin <antony.jin@epitech.eu>
2023-12-19 00:19:37 +0100

    feat(Test for conversion): Test for convert with arithmetic and define #8

--- test/AstTest.hs
+++ test/AstTest.hs
@@ -3,17 +3,31 @@ module AstTest (astTests) where
 
 import Test.HUnit
 import Lib
+import AST.Constants
+import AST.Tree.ConvertToAst
 
 astTests :: Test
-astTests = TestList [testAddition, testAdditionOne]
+astTests = TestList [arithmeticTests, specificExpressionTests]
 
+arithmeticTests :: Test
+arithmeticTests = TestList
+  [ "Test Add" ~: convertSExprToAst (SList [SSymb "+", SInt 2, SInt 3]) ~?= BinaryOp Add (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Mul" ~: convertSExprToAst (SList [SSymb "*", SInt 2, SInt 3]) ~?= BinaryOp Multiply (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Sub" ~: convertSExprToAst (SList [SSymb "-", SInt 2, SInt 3]) ~?= BinaryOp Sub (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Div" ~: convertSExprToAst (SList [SSymb "/", SInt 2, SInt 3]) ~?= BinaryOp Divide (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Mod" ~: convertSExprToAst (SList [SSymb "%", SInt 2, SInt 3]) ~?= BinaryOp Modulo (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Eq" ~: convertSExprToAst (SList [SSymb "=", SInt 2, SInt 3]) ~?= BinaryOp Equal (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test NotEq" ~: convertSExprToAst (SList [SSymb "/=", SInt 2, SInt 3]) ~?= BinaryOp NotEqual (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Less" ~: convertSExprToAst (SList [SSymb "<", SInt 2, SInt 3]) ~?= BinaryOp LessThan (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test LessOrEq" ~: convertSExprToAst (SList [SSymb "<=", SInt 2, SInt 3]) ~?= BinaryOp LessThanOrEqual (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Greater" ~: convertSExprToAst (SList [SSymb ">", SInt 2, SInt 3]) ~?= BinaryOp GreaterThan (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test GreatorEq" ~: convertSExprToAst (SList [SSymb ">=", SInt 2, SInt 3]) ~?= BinaryOp GreaterThanOrEqual (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test And" ~: convertSExprToAst (SList [SSymb "&&", SInt 2, SInt 3]) ~?= BinaryOp And (Var (AstInt 2)) (Var (AstInt 3))
+  , "Test Or" ~: convertSExprToAst (SList [SSymb "||", SInt 2, SInt 3]) ~?= BinaryOp Or (Var (AstInt 2)) (Var (AstInt 3))
+  ]
 
-
-testAddition :: Test
-testAddition = TestCase $ do
-  assertEqual "Ast Test #1" 5 (add 2 3)
-
-testAdditionOne :: Test
-testAdditionOne = TestCase $ do
-  assertEqual "Ast Test #2" 3 (add 3 3)
+specificExpressionTests :: Test
+specificExpressionTests = TestList
+  [ "Test Define" ~: convertSExprToAst (SList [SSymb "define", SSymb "x", SInt 42]) ~?= Define "x" (Var (AstInt 42))
+  ]
 
