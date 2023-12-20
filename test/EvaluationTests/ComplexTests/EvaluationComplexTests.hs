9ac0186e24a2d8b6db5f5de3de1d57ff32aacdc9
Ylan Garnier <ylan.garnier@epitech.eu>
2023-12-20 10:27:05 +0100

    feat(CI/CD): Fixing Unit Tests for Eval

--- test/EvaluationTests/ComplexTests/EvaluationComplexTests.hs
+++ test/EvaluationTests/ComplexTests/EvaluationComplexTests.hs
@@ -3,6 +3,7 @@ module EvaluationTests.ComplexTests.EvaluationComplexTests (evalComplexTests) wh
 import Test.HUnit
 import AST.Constants
 import Evaluation.Evaluation (evaluation)
+import AST.Env
 
 evalComplexTests :: Test
 evalComplexTests = TestList
@@ -10,91 +11,101 @@ evalComplexTests = TestList
 
 testComplex1 :: Test
 testComplex1 = TestCase $ do
-    assertEqual "Complex Test #1" (Just result1) (evaluation operation1)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #1" (Just expected) (result)
     where
-      a1 = BinaryOp Add (Var (AstInt 10)) (Var (AstInt 5))
-      b1 = BinaryOp Multiply (Var (AstInt 2)) (Var (AstInt 3))
-      operation1 = BinaryOp Divide a1 b1
-      result1 = Var (AstInt 2) -- (10 + 5) / (2 * 3) = 15 / 6 = 2
+      a = BinaryOp Add (Var (AstInt 10)) (Var (AstInt 5))
+      b = BinaryOp Multiply (Var (AstInt 2)) (Var (AstInt 3))
+      operation = ListOfAst [BinaryOp Divide, a, b]
+      expected = Var (AstInt 2) -- (10 + 5) / (2 * 3) = 15 / 6 = 2
 
 testComplex2 :: Test
 testComplex2 = TestCase $ do
-    assertEqual "Complex Test #2" (Just result2) (evaluation operation2)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #2" (Just expected) (result)
     where
-      a2 = BinaryOp GreaterThan (Var (AstInt 15)) (Var (AstInt 10))
-      b2 = BinaryOp LessThan (Var (AstInt 5)) (Var (AstInt 8))
-      operation2 = BinaryOp And a2 b2
-      result2 = Var (AstBool "#t") -- 15 > 10 AND 5 < 8
+      a = BinaryOp GreaterThan (Var (AstInt 15)) (Var (AstInt 10))
+      b = BinaryOp LessThan (Var (AstInt 5)) (Var (AstInt 8))
+      operation = ListOfAst [BinaryOp And, a, b]
+      expected = Var (AstBool "#t") -- 15 > 10 AND 5 < 8
 
 testComplex3 :: Test
 testComplex3 = TestCase $ do
-    assertEqual "Complex Test #3" (Just result3) (evaluation operation3)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #3" (Just expected) (result)
     where
-      a3 = BinaryOp Equal (Var (AstInt 7)) (Var (AstInt 7))
-      b3 = BinaryOp NotEqual (Var (AstInt 3)) (Var (AstInt 4))
-      operation3 = BinaryOp Or a3 b3
-      result3 = Var (AstBool "#t") -- 7 == 7 OR 3 != 4
+      a = BinaryOp Equal (Var (AstInt 7)) (Var (AstInt 7))
+      b = BinaryOp NotEqual (Var (AstInt 3)) (Var (AstInt 4))
+      operation = ListOfAst [BinaryOp Or, a, b]
+      result = Var (AstBool "#t") -- 7 == 7 OR 3 != 4
 
 testComplex4 :: Test
 testComplex4 = TestCase $ do
-    assertEqual "Complex Test #4" (Just result4) (evaluation operation4)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #4" (Just expected) (result)
     where
-      a4 = BinaryOp Modulo (Var (AstInt 20)) (Var (AstInt 6))
-      b4 = BinaryOp Sub (Var (AstInt 8)) (Var (AstInt 4))
-      operation4 = BinaryOp LessThanOrEqual a4 b4
-      result4 = Var (AstBool "#t") -- 20 % 6 >= 8
+      a = BinaryOp Modulo (Var (AstInt 20)) (Var (AstInt 6))
+      b = BinaryOp Sub (Var (AstInt 8)) (Var (AstInt 4))
+      operation = ListOfAst [BinaryOp LessThanOrEqual, a, b]
+      result = Var (AstBool "#t") -- 20 % 6 >= 8
 
 testComplex5 :: Test
 testComplex5 = TestCase $ do
-    assertEqual "Complex Test #5" (Just result5) (evaluation operation5)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #5" (Just expected) (result)
     where
-      a5 = BinaryOp Add (Var (AstInt 2)) (Var (AstInt 3))
-      b5 = BinaryOp Multiply (Var (AstInt 4)) (Var (AstInt 5))
-      c5 = BinaryOp Divide b5 (Var (AstInt 2))
-      operation5 = BinaryOp Sub a5 c5
-      result5 = Var (AstInt (-5)) -- (2 + 3) - (4 * 5 / 2) = 5 - 10 = -5
+      a = BinaryOp Add (Var (AstInt 2)) (Var (AstInt 3))
+      b = BinaryOp Multiply (Var (AstInt 4)) (Var (AstInt 5))
+      c = BinaryOp Divide b (Var (AstInt 2))
+      operation = ListOfAst [BinaryOp Sub, a, c]
+      result = Var (AstInt (-5)) -- (2 + 3) - (4 * 5 / 2) = 5 - 10 = -5
 
 testComplex6 :: Test
 testComplex6 = TestCase $ do
-    assertEqual "Complex Test #6" (Just result6) (evaluation operation6)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #6" (Just expected) (result)
     where
-      a6 = BinaryOp Add (Var (AstInt 10)) (Var (AstInt 20))
-      b6 = BinaryOp Sub (Var (AstInt 5)) (Var (AstInt 3))
-      operation6 = BinaryOp Multiply a6 b6
-      result6 = Var (AstInt 60) -- (10 + 20) * (5 - 3) = 30 * 2 = 60
+      a = BinaryOp Add (Var (AstInt 10)) (Var (AstInt 20))
+      b = BinaryOp Sub (Var (AstInt 5)) (Var (AstInt 3))
+      operation = ListOfAst [BinaryOp Multiply, a, b]
+      result = Var (AstInt 60) -- (10 + 20) * (5 - 3) = 30 * 2 = 60
 
 testComplex7 :: Test
 testComplex7 = TestCase $ do
-    assertEqual "Complex Test #7" (Just result7) (evaluation operation7)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #7" (Just expected) (result)
     where
-      a7 = BinaryOp GreaterThan (Var (AstInt 7)) (Var (AstInt 3))
-      b7 = BinaryOp LessThan (Var (AstInt 10)) (Var (AstInt 15))
-      operation7 = BinaryOp Or a7 b7
-      result7 = Var (AstBool "#t") -- 7 > 3 OR 10 < 15
+      a = BinaryOp GreaterThan (Var (AstInt 7)) (Var (AstInt 3))
+      b = BinaryOp LessThan (Var (AstInt 10)) (Var (AstInt 15))
+      operation = ListOfAst [BinaryOp Or, a, b]
+      result = Var (AstBool "#t") -- 7 > 3 OR 10 < 15
 
 testComplex8 :: Test
 testComplex8 = TestCase $ do
-    assertEqual "Complex Test #8" (Just result8) (evaluation operation8)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #8" (Just expected) (result)
     where
-      a8 = BinaryOp Equal (Var (AstInt 9)) (Var (AstInt 9))
-      b8 = BinaryOp NotEqual (Var (AstInt 10)) (Var (AstInt 10))
-      operation8 = BinaryOp And a8 b8
-      result8 = Var (AstBool "#f") -- 9 == 9 AND 10 != 10
+      a = BinaryOp Equal (Var (AstInt 9)) (Var (AstInt 9))
+      b = BinaryOp NotEqual (Var (AstInt 10)) (Var (AstInt 10))
+    operation = ListOfAst [BinaryOp And, a, b]
+      result = Var (AstBool "#f") -- 9 == 9 AND 10 != 10
 
 testComplex9 :: Test
 testComplex9 = TestCase $ do
-    assertEqual "Complex Test #9" (Just result9) (evaluation operation9)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #9" (Just expected) (result)
     where
-      a9 = BinaryOp Multiply (Var (AstInt 3)) (Var (AstInt 3))
-      b9 = BinaryOp Modulo (Var (AstInt 17)) (Var (AstInt 5))
-      operation9 = BinaryOp GreaterThanOrEqual a9 b9
-      result9 = Var (AstBool "#t") -- 9 >= 3 x 3
+      a = BinaryOp Multiply (Var (AstInt 3)) (Var (AstInt 3))
+      b = BinaryOp Modulo (Var (AstInt 17)) (Var (AstInt 5))
+      operation = ListOfAst [BinaryOp GreaterThanOrEqual, a, b]
+      result = Var (AstBool "#t") -- 9 >= 3 x 3
 
 testComplex10 :: Test
 testComplex10 = TestCase $ do
-    assertEqual "Complex Test #10" (Just result10) (evaluation operation10)
+    let (result, _) = evaluation operation createEnv
+    assertEqual "Complex Test #10" (Just expected) (result)
     where
-      a10 = BinaryOp Divide (Var (AstInt 100)) (Var (AstInt 5))
-      b10 = BinaryOp Multiply (Var (AstInt 3)) (Var (AstInt 2))
-      operation10 = BinaryOp Sub a10 b10
-      result10 = Var (AstInt 14) -- (100 / 5) - (3 * 2) = 20 - 6 = 14
+      a = BinaryOp Divide (Var (AstInt 100)) (Var (AstInt 5))
+      b = BinaryOp Multiply (Var (AstInt 3)) (Var (AstInt 2))
+      operation = ListOfAst [BinaryOp Sub, a, b]
+      result = Var (AstInt 14) -- (100 / 5) - (3 * 2) = 20 - 6 = 14
