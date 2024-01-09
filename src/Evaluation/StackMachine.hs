2d07af6dacc696020417ca596419a444abbdcedf
jouzep <josephyu31@gmail.com>
2024-01-10 00:52:01 +0100

    feat(): JumpIfFalse

--- src/Evaluation/StackMachine.hs
+++ src/Evaluation/StackMachine.hs
@@ -2,7 +2,7 @@ data Value = IntVal Int | BoolVal Bool deriving (Show)
 
 data Operator = Add | Subtract | Multiply | Divide | Eq | Less deriving (Show)
 
-data Instruction = Push Value | Call Operator | Ret | JumpIfFalse deriving (Show)
+data Instruction = Push Value | Call Operator | Ret | JumpIfFalse Value deriving (Show)
 
 type Stack = [Value]
 type Insts = [Instruction]
@@ -14,9 +14,10 @@ exec (Call op : insts) stack = do
     case stack of 
         (IntVal v2 : (IntVal v1 : rest)) -> execOperator op insts stack
         _ -> Left $ "Error: " ++ show op ++ "Operator call needs two integer arguments"
-exec (JumpIfFalse : insts) (BoolVal False : stack) = exec insts stack
+exec (JumpIfFalse (IntVal number) : insts) (BoolVal False : stack) = exec (drop number insts) stack
+exec (JumpIfFalse (IntVal number) : insts) (BoolVal True : stack) = exec (insts) stack
 exec (Ret : _) [result] = Right result
-exec _ _ = Left "Invalid operation or insufficient operands"
+exec insts stacks = Left $ "Invalid operation or insufficient operands" ++ show insts ++ show stacks
 
 
 execOperator :: Operator -> Insts -> Stack -> Either String Value
@@ -40,9 +41,10 @@ main = do
     let program2 = [Push (IntVal 0), Push (IntVal 41), Call Divide, Ret]
     let program3 = [Push (IntVal 10), Push (IntVal 10), Call Eq, Ret]
     let program4 = [Push (IntVal 2), Push (IntVal 5), Call Less, Ret]
-    let program5 = [Push (IntVal 2), Push (IntVal 5), Call Eq, JumpIfFalse, Push (IntVal 1), Ret, Push (IntVal 2), Ret]
+    -- let program5 = [Push (IntVal 2), Push (IntVal 5), Call Eq, JumpIfFalse, Push (IntVal 1), Ret, Push (IntVal 2), Ret]
+    let program6 = [Push (IntVal 11), Push (IntVal 10), Call Eq, JumpIfFalse (IntVal 2), Push (IntVal 1), Ret, Push (IntVal 2), Ret]
     putStrLn $ printResult (exec program1 [])
     putStrLn $ printResult (exec program2 [])
     putStrLn $ printResult (exec program3 [])
     putStrLn $ printResult (exec program4 [])
-    putStrLn $ printResult (exec program5 [])
+    putStrLn $ printResult (exec program6 [])
