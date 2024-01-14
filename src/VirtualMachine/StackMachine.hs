5c95d178f7eadb25ea3f0920ff3ddb0b119f15a6
Ylan Garnier <ylan.garnier@epitech.eu>
2024-01-14 04:03:25 +0100

    feat(VM): fix stackMachine and test every aspect

--- src/VirtualMachine/StackMachine.hs
+++ src/VirtualMachine/StackMachine.hs
@@ -1,6 +1,6 @@
-module VirtualMachine.StackMachine (exec, execOperator, execGetFunctionFromEnv, printResult) where
+module StackMachine (exec, execOperator, execGetFunctionFromEnv, printResult) where
 
-import VirtualMachine.VMConstants
+import VMConstants
 
 exec :: Env -> Args -> Insts -> Stack -> Either String Value
 exec env args [] [result] = Right result
@@ -17,7 +17,9 @@ exec env args (Call : insts) (OpVal op : stack) = execOperator env args (OpVal o
 exec env args (Call : _) _ = Left "Call instruction requires a function or operator on the stack"
 exec env args (JumpIfFalse (IntVal number) : insts) (BoolVal False : stack) = exec env args (drop number insts) stack
 exec env args (JumpIfFalse (IntVal number) : insts) (BoolVal True : stack) = exec env args (insts) stack
+exec env args (JumpIfFalse (IntVal number) : insts) (_ : stack) = Left "Conditions requires a boolean on the stack"
 exec env args (Ret : _) (result : _) = Right result
+exec env args (Ret : _) _ = Left "Return need a value on the stack"
 exec env args (PushArg index : insts) stack
     | index < length args = exec env args insts ((args !! index) : stack)
     | otherwise = Left $ "Invalid argument index: " ++ show index
@@ -44,45 +46,44 @@ printResult (Right (BoolVal val)) = show val
 printResult (Right (IntVal val)) = show val
 printResult (Left errMsg) = errMsg
 
--- main :: IO ()
--- main = do
---     let args = [IntVal 42, IntVal (-42)]
---     let function = [PushArg 0, Push (IntVal 0), Push (OpVal Less1), Call, JumpIfFalse (IntVal 2), PushArg 0, Ret, PushArg 0, Push (IntVal (-1)), Push (OpVal Multiply1), Call, Ret]
---     let program1 = [Push (IntVal 42), Ret]
---     let program2 = [Push (IntVal 0), Push (IntVal 41), Push (OpVal Divide1), Call, Ret]
---     let program3 = [Push (IntVal 10), Push (IntVal 10), Push (OpVal Eq1), Call, Ret]
---     let program4 = [Push (IntVal 2), Push (IntVal 5), Push (OpVal Less1), Call, Ret]
---     let program5 = [Push (IntVal 2), Push (IntVal 2), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 1), Push (IntVal 1), Ret, Push (IntVal 4), Ret]
---     let program6 = [Push (IntVal 11), Push (IntVal 10), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 2), Push (IntVal 1), Ret, Push (IntVal 2), Ret]
---     let programWithArgs = [PushArg 1, Push (IntVal 0), Push (OpVal Less1), Call, JumpIfFalse (IntVal 2), PushArg 1, Ret, PushArg 0, Push (IntVal (-1)), Push (OpVal Multiply1), Call, Ret]
---     let programWithUserFunc = [Push (IntVal (-42)), Push (FuncVal function), Call,Push (FuncVal function), Call, Push (IntVal 5), Push (OpVal Eq1), Call,JumpIfFalse (IntVal 2), Push (IntVal 1), Ret, Push (IntVal 2), Ret]
---     let env = [("fact", Push (FuncVal[PushArg 0, Push(IntVal 1), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 2), Push (IntVal 1), Ret, Push (IntVal 1), PushArg 0, Push(OpVal Subtract1), Call, PushEnv "fact", Call, PushArg 0, Push (OpVal Multiply1), Call , Ret]))]
---                                  --   PushArg 0  Push 1          Push Eq          Call  JumpIfFalse 2           Push 1           Ret  Push 1           PushArg 0  Push Sub              Call  PushEnv "fact"  Call  PushArg 0  Push Mul               Call   Ret
---     let programWithEnvAndRecursive = [Push (IntVal 5), PushEnv "fact", Call, Ret]
---     putStrLn $ "test 1: " ++ printResult (exec [] args program1 [])
---     putStrLn $ "test 2: " ++ printResult (exec [] args program2 [])
---     putStrLn $ "test 3: " ++ printResult (exec [] args program3 [])
---     putStrLn $ "test 4: " ++ printResult (exec [] args program4 [])
---     putStrLn $ "test 5: " ++ printResult (exec [] args program5 [])
---     putStrLn $ "test 6: " ++ printResult (exec [] args program6 [])
---     putStrLn $ "test Args: " ++ printResult (exec [] args programWithArgs [])
---     putStrLn $ "test UserFunc: " ++ printResult (exec [] [] programWithUserFunc [])
---     putStrLn $ "test Env And Recursive Function: " ++ printResult (exec env [] programWithEnvAndRecursive [])
-
--- [0, -42]
--- [0, -42]
-
--- 0 < -42
--- False
--- [Mul, -1, -42, 0, -42]
-
--- main(a, b) {
---     func1(a b) {
---         let c = a + b
---         let a = func2(c) {
---             c vient de la stack ||  stack: [] args: []
---             let d = c + 1
---             let e = c + 2
---         }
---     }
--- }
+main :: IO ()
+main = do
+    let args = [IntVal 42, IntVal (-42)]
+    let function = [PushArg 0, Push (IntVal 0), Push (OpVal Less1), Call, JumpIfFalse (IntVal 2), PushArg 0, Ret, PushArg 0, Push (IntVal (-1)), Push (OpVal Multiply1), Call, Ret]
+    let env = [("fact", Push (FuncVal[PushArg 0, Push(IntVal 1), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 2), Push (IntVal 1), Ret, Push (IntVal 1), PushArg 0, Push(OpVal Subtract1), Call, PushEnv "fact", Call, PushArg 0, Push (OpVal Multiply1), Call , Ret]))]
+    let programDivide1 = [Push (IntVal 0), Push (IntVal 42), Push (OpVal Divide1), Call, Ret]
+    let programDivide2 = [Push (IntVal 2), Push (IntVal 42), Push (OpVal Divide1), Call, Ret]
+    let programMultiply = [Push (IntVal 3), Push (IntVal 10), Push (OpVal Multiply1), Call, Ret]
+    let programEqual1 = [Push (IntVal 10), Push (IntVal 10), Push (OpVal Eq1), Call, Ret]
+    let programEqual2 = [Push (IntVal 10), Push (IntVal 11), Push (OpVal Eq1), Call, Ret]
+    let programLess1 = [Push (IntVal 2), Push (IntVal 5), Push (OpVal Less1), Call, Ret]
+    let programLess2 = [Push (IntVal 5), Push (IntVal 2), Push (OpVal Less1), Call, Ret]
+    let programEq1 = [Push (IntVal 5), Push (IntVal 5), Push (OpVal Eq1), Call, Ret]
+    let programEq2 = [Push (IntVal 2), Push (IntVal 5), Push (OpVal Eq1), Call, Ret]
+    let programWithArgs1 = [PushArg 1, Ret]
+    let programWithArgs2 = [PushArg 3, Ret]
+    let programWithUserFunc = [Push (IntVal (-42)), Push (FuncVal function), Call, Push (OpVal Eq1), Call, Ret]
+    let programWithEnvAndRecursive = [Push (IntVal 5), PushEnv "fact", Call, Ret]
+    let programRet1 = [Push (IntVal 42), Ret]
+    let programRet2 = [Ret]
+    let programTestJumpIfFalse1 = [Push (IntVal 5), Push (IntVal 5), JumpIfFalse (IntVal 1), Push (IntVal 3), Ret]
+    let programTestJumpIfFalse2 = [Push (IntVal 5), Push (IntVal 5), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 2), Push (IntVal 3), Ret, Push (IntVal 1), Ret]
+    let programTestJumpIfFalse3 = [Push (IntVal 5), Push (IntVal 6), Push (OpVal Eq1), Call, JumpIfFalse (IntVal 2), Push (IntVal 3), Ret, Push (IntVal 1), Ret]
+    putStrLn $ "test Divide 42 / 0: \n" ++ printResult (exec [] args programDivide1 []) ++ "\n"
+    putStrLn $ "test Divide 42 / 2: \n" ++ printResult (exec [] args programDivide2 []) ++ "\n"
+    putStrLn $ "test Multiply 3 * 10: \n" ++ printResult (exec [] args programMultiply []) ++ "\n"
+    putStrLn $ "test Equal true: \n" ++ printResult (exec [] args programEqual1 []) ++ "\n"
+    putStrLn $ "test Equal false: \n" ++ printResult (exec [] args programEqual2 []) ++ "\n"
+    putStrLn $ "test Less: 5 < 2: \n" ++ printResult (exec [] args programLess1 []) ++ "\n"
+    putStrLn $ "test Less: 2 < 5:\n" ++ printResult (exec [] args programLess2 []) ++ "\n"
+    putStrLn $ "test Equal true:\n" ++ printResult (exec [] args programEq1 []) ++ "\n"
+    putStrLn $ "test Equal false:\n" ++ printResult (exec [] args programEq2 []) ++ "\n"
+    putStrLn $ "test Return Ok: \n" ++ printResult (exec [] args programRet1 []) ++ "\n"
+    putStrLn $ "test Return error empty stack: \n" ++ printResult (exec [] args programRet2 []) ++ "\n"
+    putStrLn $ "test JumpIfFalse should error no boolean on stack: \n" ++ printResult (exec [] [] programTestJumpIfFalse1 []) ++ "\n"
+    putStrLn $ "test JumpIfFalse should return 3: \n" ++ printResult (exec [] [] programTestJumpIfFalse2 []) ++ "\n"
+    putStrLn $ "test JumpIfFalse should return 1: \n" ++ printResult (exec [] [] programTestJumpIfFalse3 []) ++ "\n"
+    putStrLn $ "test Args ok: \n" ++ printResult (exec [] args programWithArgs1 []) ++ "\n"
+    putStrLn $ "test Args index error: \n" ++ printResult (exec [] args programWithArgs2 []) ++ "\n"
+    putStrLn $ "test UserFunc: \n" ++ printResult (exec [] [] programWithUserFunc []) ++ "\n"
+    putStrLn $ "test Env And Recursive Function: \n" ++ printResult (exec env [] programWithEnvAndRecursive []) ++ "\n"
